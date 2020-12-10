b6826d24126efd621fe17076a7dbd9208c4e0353
Lukasz Momot <lukasz.momot@hotmail.com>
2020-12-10 18:45:53 +0100

    Add comment to method

--- cmd/httpserver.go
+++ cmd/httpserver.go
@@ -20,6 +20,7 @@ func main() {
 	}
 }
 
+// HealthHandler handles health check requests
 func HealthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Digital Broker Finance Microservice is up and running")
