f6e6ffe96d52e613e329daa9eb352b90b4bdf588
Lukasz Momot <lukasz.momot@hotmail.com>
2020-12-11 13:46:55 +0100

    Load env variables

--- cmd/httpserver.go
+++ cmd/httpserver.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
-	"github.com/lukasmomot/dbfinance/rest"
 	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
+	"github.com/lukasmomot/dbfinance/rest"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	var err error
 	fmt.Println("Digital Broker finance microservice is starting on port 3002")
+	// Load configuration
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", HealthHandler).Methods("GET")
 	r.HandleFunc("/health", HealthHandler).Methods("GET")
@@ -22,7 +31,7 @@ func main() {
 			"amount", "{amount}").
 		Methods("GET")
 
-	err := http.ListenAndServe(":3002", r)
+	err = http.ListenAndServe(":3002", r)
 	if err != nil {
 		log.Fatal("Cannot start http server...")
 	}
--- services/currency/currency.go
+++ services/currency/currency.go
@@ -1,9 +1,14 @@
 package currency
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 // ConvertCurrency converts the currency
 func ConvertCurrency(from string, to string, amount float64) float64 {
 	fmt.Printf("From: %s, to: %s", from, to)
+	apiKey := os.Getenv("FIXER_API_KEY")
+	fmt.Printf("FIXER key for api: %s", apiKey)
 	return amount * 4
 }
